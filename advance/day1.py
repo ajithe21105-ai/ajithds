1ca1935f0742dd2294055ec51818a74d6cadeebb
ajithe111111 <ajithe21105>
2026-04-17 17:50:29 +0530

    adding folder advance

---
+++ advance/day1.py
@@ -0,0 +1,107 @@
+# Oops intro task :-- 
+
+# 🧩 Task 1: Student System
+# 👉 Question:
+# Create a class Student with:
+# Class variable: school_name = "XYZ School"
+# A method set_details()
+#  → inside method, assign:
+# name = "Vamsi"
+# marks = 85
+# A method display()
+#  → print:
+# Name
+# Marks
+# School name
+# 👉 Outside the class:
+# Create object
+# Call set_details()
+# Call display()
+
+
+
+
+
+class student():
+    school_name = "xyz school"
+
+    def set_details():
+
+        name = "vamsi"
+        marks = 85
+
+    def display():
+    
+        print(name)
+        print(marks)
+        print(school name)
+o=student
+o.set_details()
+o.display()
+
+
+
+
+
+# 🧩 Task 2: Employee System
+# 👉 Question:
+# Create a class Employee with:
+# Class variable: company = "Infosys"
+# A method set_data()
+#  → assign:
+# name = "Ravi"
+# salary = 20000
+# A method increase_salary()
+#  → add 5000 to salary
+# A method display()
+#  → print all details
+# 👉 Outside the class:
+# Create object
+# Call all methods
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 🧩 Task 3: Mobile System
+# 👉 Question:
+# Create a class Mobile with:
+# Class variable: brand = "Apple"
+# A method set_details()
+#  → assign:
+# model = "iPhone 14"
+# price = 80000
+# A method discount()
+#  → reduce price by 10%
+# A method show_details()
+#  → print all details
+# 👉 Outside the class:
+# Create object
+# Call methods
