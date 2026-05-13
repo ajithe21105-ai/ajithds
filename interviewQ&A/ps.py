886d7039aa3d9555634a8ceab576ae978cb43f20
ajithe111111 <ajithe21105>
2026-05-13 16:38:31 +0530

    adding modified clean & pfractice code

---
+++ interviewQ&A/ps.py
@@ -0,0 +1,52 @@
+# a=[2,3,4,2,3,4,5,7]
+# o=[]
+# for i in a:
+#     if i not in o:
+#         o.append(i)
+# print(o)
+
+
+
+# a=[1,2,2,2,3,4,4,4,5,6,7,8]
+# o={}
+# for i in a:
+#     if i not in o:
+#         o[i]=1
+#     else:
+#         o[i]+=1
+# print(o)
+
+
+
+# a=[1,2,3,4,5,6,7,8]
+# o=[]
+# for i in range(len(a)-1,-1,-1):
+#     if a[i]>0:
+#         o.append(a[i])
+# print("reverse list",o)
+
+
+
+# a="pythonprogramming"
+# count = 0
+# for i in a:
+#     if i in "aeiou":
+#         count += 1
+# print(count)
+
+
+
+# l=[1,2,3,4]
+# a=0
+# for i in l:
+#         a+=i
+# print(a)
+
+
+
+a=10
+b=20
+a,b=b,a
+print(a,"a")
+print(b,"b")
+
